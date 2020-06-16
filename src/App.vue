b833e270e01e2a9dfd15f12b88dc26072ea5b0a1
derrisahputra12 <derrisahputra@gmail.com>
2020-06-16 18:06:43 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <p>Nama Saya Deri sahputra</p>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "App",
+  data() {
+    return {
+    };
+  },
+
+};
+</script>
+
+<style lang="scss"></style>
